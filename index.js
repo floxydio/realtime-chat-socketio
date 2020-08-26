243d8375361d9b894bc2cab230da5429a9e8c0d0
floxydio <63448336+floxydio@users.noreply.github.com>
2020-08-26 22:43:30 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const { Socket } = require('dgram');
+
+let app = require('express')();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+http.listen(3000, () => {
+    console.log('Connected done');
+  });
+
+io.on('connection', (socket) => {
+  console.log('There is a Connection');
+
+  socket.on('disconnection', () => {
+      console.log('Disconnect');
+    })
+
+    socket.on('Created', (data) => {
+        socket.broadcast.emit('Created', (data))
+    })
+
+    socket.on('chat-message', (data) => {
+        socket.broadcast.emit('chat-message', (data))
+    })
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', (data))
+    })
+    socket.on('stoptyping', (data) => {
+        socket.broadcast.emit('stoptyping', (data))
+    })
+
+    socket.on('joined', (name) => {
+        socket.broadcast.emit('joined', (name))
+    })
+    socket.on('leaved', (name) => {
+      socket.broadcast.emit('leaved', (name))
+  })
+
+});
